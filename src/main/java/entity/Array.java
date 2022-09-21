88d3c6e50064850da24b9dc4282b311a35f33f4f
AlexNiunko <alexniunko89@gmail.com>
2022-09-21 19:01:29 +0300

    homeWork

--- src/main/java/entity/Array.java
+++ src/main/java/entity/Array.java
@@ -43,6 +43,22 @@ public class Array {
         return  (i>=0 && i<Arr.length);
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Array array = (Array) o;
+
+        return Arrays.equals(Arr, array.Arr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(Arr);
+    }
+
     @Override
     public String toString() {
         return "Array{" +
