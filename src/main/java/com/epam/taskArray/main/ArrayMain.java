b3092bf105df24117dea7ca14e31ffb83ad9205e
AlexNiunko <alexniunko89@gmail.com>
2022-09-28 19:51:46 +0300

    TaskArray

--- src/main/java/com/epam/taskArray/main/ArrayMain.java
+++ src/main/java/com/epam/taskArray/main/ArrayMain.java
@@ -3,9 +3,14 @@ package com.epam.taskArray.main;
 import com.epam.taskArray.exception.ArrayException;
 import com.epam.taskArray.reader.ArrayFileReader;
 import com.epam.taskArray.validator.ArrayValidator;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 
 public class ArrayMain {
+    static final Logger logger= (Logger) LogManager.getLogger();
     public static void main(String[] args) throws ArrayException {
+        logger.log(Level.INFO,"start program");
         ArrayFileReader arrayFileReader=new ArrayFileReader();
         String str=arrayFileReader.readFromFile("data\\info.txt");
         System.out.println(str);
